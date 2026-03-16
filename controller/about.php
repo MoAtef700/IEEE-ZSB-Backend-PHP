6aeb2a736fcc6ed4f6b596d6fdf0b51ab4841217
MoAtef700 <moatef7308@gmail.com>
2026-03-16 04:15:28 +0200

    Add PHP MVC project

---
+++ controller/about.php
@@ -0,0 +1,3 @@
+<?php
+$heading = 'About mooooooooooo';
+require 'views/about.view.php';
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'functions.php';
+require 'Database.php';
+
+$config = require ('config.php');
+
+   $db = new Database($config['database']);
+
+   $id = $_GET['id'];
+   $query = "SELECT * FROM posts WHERE id = :id";
+
+   $posts = $db->query($query,[':id' => $id])->fetch();
